6d54b50478d9c0f94234bb219d50792f53311fec
nbbhatia <nidhibhatia7078@gmail.com>
2021-10-04 13:14:02 +0530

    scroll promos

--- src/components/home/components/Promos.js
+++ src/components/home/components/Promos.js
@@ -1,16 +1,27 @@
 import React from "react";
 import { Grid } from "@material-ui/core";
-// import { Slide } from "react-slideshow-image";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from "react-responsive-carousel";
 const Promos = (promoData) => {
   let banners = promoData?.promoData;
 
   return (
     <Grid md={12} xs={12} sm={12} item container>
-      {banners?.map((obj) => (
-        <Grid md={6} xs={12} sm={12} item>
-          <img src={`https://dinenite.in/${obj.photo_url}`} width="100%" />
-        </Grid>
-      ))}
+      <Carousel
+        autoPlay={true}
+        showThumbs={false}
+        showArrows={false}
+        infiniteLoop={true}
+        labels={false}
+        showIndicators={false}
+        showStatus={false}
+      >
+        {banners?.map((obj) => (
+          <Grid md={12} xs={12} sm={12} item>
+            <img src={`https://dinenite.in/${obj.photo_url}`} width="100%" />
+          </Grid>
+        ))}
+      </Carousel>
     </Grid>
   );
 };
